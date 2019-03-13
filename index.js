5fa65f655a6b8ea43bd630a1db2d4b615053eba4
ttuom1nen <ttuominen@outlook.com>
2019-03-13 20:35:04 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send({hi:'there'})
+})
+
+//Dynamic port for Heroku or 5000
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
