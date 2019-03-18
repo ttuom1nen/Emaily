a8b95c74878f09010d6788c72fd4b081fa150c70
ttuom1nen <ttuominen@outlook.com>
2019-03-18 19:42:40 +0200

    Check if OAuthed user has already been added to db

--- services/passport.js
+++ services/passport.js
@@ -13,8 +13,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      //console.log("profile", profile);
-      new User({ googleId: profile.id }).save();
+      User.findone({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
