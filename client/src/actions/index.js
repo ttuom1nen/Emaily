43901624b600abc1589e51a9158d06d5f6ad586a
ttuom1nen <ttuominen@outlook.com>
2019-03-23 17:36:39 +0200

    Axios/Thunk

---
+++ client/src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { FETCH_USER } from "./types";
+
+const fetchUser = () => {
+  return function() {
+    axios
+      .get("/api/current_user")
+      .then(res => dispatchEvent({ type: FETCH_USER, payload: res }));
+  };
+};
