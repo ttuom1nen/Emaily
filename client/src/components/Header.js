2d6ee43a17ece137f3114942ec275c072944d672
ttuom1nen <ttuominen@outlook.com>
2019-03-23 10:14:53 +0200

    Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,20 @@ import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper blue darken-1">
+          <a href="#" className="brand-logo">
+            Emaily
+          </a>
+          <ul id="nav-mobile" className="right">
+            <li>
+              <a href="#">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
