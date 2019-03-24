3b8ab11f9911c0a513c0e1a835b85890a40835b5
ttuom1nen <ttuominen@outlook.com>
2019-03-24 21:22:24 +0200

    Fixed Google auth bug

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,14 +1,6 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
 
-export default function(state = {}, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
-
-/*
 export default combineReducers({
   auth: authReducer
-});*/
+});
