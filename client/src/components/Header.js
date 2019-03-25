215a9046af8751c8e5e64d1740f7cfae69b98759
ttuom1nen <ttuominen@outlook.com>
2019-03-25 20:53:28 +0200

    Added stripe checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   renderContent() {
@@ -27,9 +28,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper blue darken-1">
-          <a href="#" className="brand-logo">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
           <ul id="nav-mobile" className="right">
             {this.renderContent()}
           </ul>
