52fb4105fee2c471c9a68f2ef4616b86fe66a834
ttuom1nen <ttuominen@outlook.com>
2019-03-28 21:13:05 +0200

    Added body parser

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,1 +1,6 @@
-module.exports = app => {};
+const keys = require("../config/keys");
+const stripe = require("stripe")(keys.stripeSecretKey);
+
+module.exports = app => {
+  app.post("/api/stripe", (req, res) => {});
+};
