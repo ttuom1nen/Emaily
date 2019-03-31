ac090468136e33679b8314c10f3a7c698c83bf16
ttuom1nen <ttuominen@outlook.com>
2019-03-31 22:56:08 +0300

    Check user login middleware

---
+++ middlewares/requireLogin.js
@@ -0,0 +1,11 @@
+/**
+ * Check if user is logged in
+ */
+
+module.exports = (req, res, next) => {
+  if (!req.user) {
+    return res.status(401).send({ error: "You must log in!" });
+  }
+
+  next();
+};
