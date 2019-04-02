826d59ab208353e28fb58bebe8f107a69af5bde3
ttuom1nen <ttuominen@outlook.com>
2019-04-02 21:33:24 +0300

    Production routing

--- index.js
+++ index.js
@@ -27,6 +27,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express serves production assets
+  app.use(express.static("client/build"));
+
+  // Express serves index.html if
+  // route not recognized
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 //Dynamic port for Heroku or 5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
