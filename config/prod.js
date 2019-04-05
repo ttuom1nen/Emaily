8f7e103be8edb0bf0f22397f7e69bf49395d58b3
ttuom1nen <ttuominen@outlook.com>
2019-04-05 23:14:07 +0300

    Debug logs

--- config/prod.js
+++ config/prod.js
@@ -5,6 +5,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  dummy: "For Heroku push"
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
