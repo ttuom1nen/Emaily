6b3970f2bee9a1a3e0d645b66226164adfbb12fe
ttuom1nen <ttuominen@outlook.com>
2019-04-11 23:27:43 +0300

    Mailer.js continued

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,4 +5,15 @@ const keys = require("../config/keys");
 /**
  * Extend the sendgrid.mail
  */
-class Mailer extends helper.Mail {}
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email("no-reply@emaily.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+  }
+}
+
+module.exports = Mailer;
