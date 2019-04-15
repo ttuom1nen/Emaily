bb0da63342ac615dc5e0264a3235e0d2f4b627f1
ttuom1nen <ttuominen@outlook.com>
2019-04-15 23:51:58 +0300

    Redux-forms added + new 'surveys' components

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,17 @@
+/**
+ * SurveyNew shows survey input for creating a new survey
+ */
+import React, { Component } from "react";
+import SurveyForm from "./SurveyForm";
+
+class SurveyNew extends Component {
+  render() {
+    return (
+      <div>
+        <SurveyForm />
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
