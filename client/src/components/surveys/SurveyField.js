1c73d53f9183bf976558e03283a2641872bea54d
ttuom1nen <ttuominen@outlook.com>
2019-04-18 22:06:31 +0300

    Validation styling

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,13 +1,19 @@
 // SurveyField - render a single label on text input
 import React from "react";
 
+/* meta is from redux form */
 export default ({ input, label, meta }) => {
   console.log(meta);
   return (
     <div>
       <label>{label}</label>
-      <input {...input} />
-      {meta.touched && meta.error /*if touched && error*/}
+      <input {...input} style={{ marginBottom: "5px" }} />
+      <div
+        className="red-text"
+        style={{ fontSize: "0.8rem", marginBottom: "20px" }}
+      >
+        {meta.touched && meta.error /*if touched && error*/}
+      </div>
     </div>
   );
 };
