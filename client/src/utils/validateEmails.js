093fd150592d81280aafa7b05d50f094aa2af615
ttuom1nen <ttuominen@outlook.com>
2019-04-19 16:26:28 +0300

    Form email validation

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,3 +1,18 @@
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export default emails => {
-  const emailsArray = emails.split(",").map(email => email.trim());
+  const emailsArray = emails
+
+    //Split the emails string
+    .split(",")
+
+    //Trim spaces from emails string
+    .map(email => email.trim())
+
+    //Filter out the emails that are invalid
+    .filter(email => regex.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `these emails are invalid: ${invalidEmails}`;
+  }
 };
