43b7badcf1f08ed7e474f2b38f78d55a62ee1a1f
ttuom1nen <ttuominen@outlook.com>
2019-04-19 16:29:26 +0300

    Bug fix

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,7 +1,7 @@
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 export default emails => {
-  const emailsArray = emails
+  const invalidEmails = emails
 
     //Split the emails string
     .split(",")
