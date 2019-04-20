455c25e1b396091ce84fd99d21b703dbbd09fcca
ttuom1nen <ttuominen@outlook.com>
2019-04-20 22:50:18 +0300

    Form fields refactor, split to separate file

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,10 +2,11 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
+
       <button className="yellow darken-1 btn-flat" onClick={onCancel}>
         Back
         <i className="material-icons left">arrow_back</i>
