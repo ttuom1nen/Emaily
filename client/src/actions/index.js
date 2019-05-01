03baf3d75df184164b3995f3d79c4f7ba04b816d
ttuom1nen <ttuominen@outlook.com>
2019-05-01 22:25:38 +0300

    Surveys reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { FETCH_USER, FETCH_SURVEYS } from "./types";
 
 /**
  * Redux action creators
@@ -7,17 +8,22 @@ import axios from "axios";
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
-  dispatch({ type: "fetch_user", payload: res.data });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const handleToken = token => async dispatch => {
   const res = axios.post("/api/stripe", token);
-  dispatch({ type: "fetch_user", payload: res.data });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post("/api/surveys", values);
 
   history.push("/surveys");
-  dispatch({ type: "fetch_user", payload: res.data });
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
