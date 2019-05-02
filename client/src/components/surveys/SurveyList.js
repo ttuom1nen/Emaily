b4a1215d957d3aafb470aad94fd37ca64de80fe8
ttuom1nen <ttuominen@outlook.com>
2019-05-02 22:32:07 +0300

    Survey listing and styling

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,8 +7,28 @@ class SurveyList extends Component {
     this.props.fetchSurveys();
   }
 
+  renderSurveys() {
+    return this.props.surveys.map(survey => {
+      return (
+        <div className="card grey lighten-5" key={survey._id}>
+          <div className="card-content">
+            <span className="card-title">{survey.title}</span>
+            <p>{survey.body}</p>
+            <p className="right">
+              Sent on: {new Date(survey.dateSent).toLocaleDateString()}
+            </p>
+          </div>
+          <div className="card-action">
+            <a>Yes: {survey.yes}</a>
+            <a>No: {survey.no}</a>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>SurveyList</div>;
+    return <div>{this.renderSurveys()}</div>;
   }
 }
 
