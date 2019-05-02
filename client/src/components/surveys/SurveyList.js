e79e54d8c67b3e4af0dd40e2d70e602a0a971f6b
ttuom1nen <ttuominen@outlook.com>
2019-05-02 22:42:50 +0300

    Minor changes

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,7 +8,7 @@ class SurveyList extends Component {
   }
 
   renderSurveys() {
-    return this.props.surveys.map(survey => {
+    return this.props.surveys.reverse().map(survey => {
       return (
         <div className="card grey lighten-5" key={survey._id}>
           <div className="card-content">
